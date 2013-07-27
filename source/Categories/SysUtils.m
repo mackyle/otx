f31fa5ddccee751f235095fa7afd5662065a32de
Ant <ant@dervishsoftware.com>
2013-07-27 17:35:48 +0100

    Added more flexible search for command-line tools.
    Committed with numerous changes from original patch.
    
    Original-patch-from: Ant <ant@dervishsoftware.com>

--- source/Categories/SysUtils.m
+++ source/Categories/SysUtils.m
@@ -12,33 +12,52 @@
 
 @implementation NSObject(SysUtils)
 
-//  checkOtool:
+//  searchForCommand:additionalPaths:
 // ----------------------------------------------------------------------------
 
-- (BOOL)checkOtool: (NSString*)filePath
+- (NSString *)searchForCommand: (NSString *)command additionalPaths: (NSArray *)additionalPaths
 {
-    NSString* otoolPath = [self pathForTool: @"otool"];
-    NSTask* otoolTask = [[[NSTask alloc] init] autorelease];
-    NSPipe* silence = [NSPipe pipe];
+    NSString *commandPath = nil;
 
-    [otoolTask setLaunchPath: otoolPath];
-    [otoolTask setStandardInput: [NSPipe pipe]];
-    [otoolTask setStandardOutput: silence];
-    [otoolTask setStandardError: silence];
-    [otoolTask launch];
-    [otoolTask waitUntilExit];
+    if (![command isAbsolutePath]) {
+        // Next, search the PATH enviornment variable for the script
 
-    return ([otoolTask terminationStatus] == 1);
+        NSMutableArray *paths;
+        const char *pathEnv = getenv("PATH");
+        if (pathEnv) {
+            NSString *pathStr = [NSString stringWithCString: pathEnv encoding: NSUTF8StringEncoding];
+            paths = [NSMutableArray arrayWithArray: [pathStr componentsSeparatedByString: @":"]];
+        } else {
+            paths = [NSMutableArray array];
+        }
+
+        if (additionalPaths != nil)
+            [paths addObjectsFromArray: additionalPaths];
+
+        NSFileManager* fileManager = [[[NSFileManager alloc] init] autorelease];
+
+        NSUInteger i, pathsCount = [paths count];
+        for (i = 0; i < pathsCount; i++) {
+            NSString *path = (NSString *)[paths objectAtIndex: i];
+            NSString* absPath = [path stringByAppendingPathComponent: command];
+
+            if ([fileManager fileExistsAtPath: absPath]) {
+                commandPath = absPath;
+                break;
+            }
+        }
+    }
+
+    return commandPath;
 }
 
-//  pathForTool:
+//  pathForXcodeTools
 // ----------------------------------------------------------------------------
 
-- (NSString*)pathForTool: (NSString*)toolName
+- (NSString *)pathForXcodeTools
 {
-    NSString* relToolBase = [NSString pathWithComponents:
-        [NSArray arrayWithObjects: @"/", @"usr", @"bin", nil]];
-    NSString* relToolPath = [relToolBase stringByAppendingPathComponent: toolName];
+    NSString *relToolBase =
+        [NSString pathWithComponents: [NSArray arrayWithObjects: @"/", @"usr", @"bin", nil]];
     NSString* selectToolPath = [relToolBase stringByAppendingPathComponent: @"xcode-select"];
     NSTask* selectTask = [[[NSTask alloc] init] autorelease];
     NSPipe* selectPipe = [NSPipe pipe];
@@ -54,16 +73,48 @@
     int selectStatus = [selectTask terminationStatus];
 
     if (selectStatus == -1)
-        return relToolPath;
+        return nil;
 
     NSData* selectData = [[selectPipe fileHandleForReading] availableData];
     NSString* absToolPath = [[[NSString alloc] initWithBytes: [selectData bytes]
                                                       length: [selectData length]
                                                     encoding: NSUTF8StringEncoding] autorelease];
 
-    return [[absToolPath stringByTrimmingCharactersInSet:
-        [NSCharacterSet whitespaceAndNewlineCharacterSet]]
-        stringByAppendingPathComponent: relToolPath];
+    return [absToolPath stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+//  pathForTool:
+// ----------------------------------------------------------------------------
+
+- (NSString *)pathForTool: (NSString *)toolName
+{
+    NSString *xcodeToolPath = [self pathForXcodeTools];
+    return [self searchForCommand: toolName
+                  additionalPaths: xcodeToolPath
+                                   ? [NSArray arrayWithObject: [self pathForXcodeTools]]
+                                   : nil];
+}
+
+//  checkOtool:
+// ----------------------------------------------------------------------------
+
+- (BOOL)checkOtool: (NSString*)filePath
+{
+    NSString *otoolPath = [self pathForTool: @"otool"];
+    if (!otoolPath)
+        return NO;
+
+    NSTask *otoolTask = [[[NSTask alloc] init] autorelease];
+    NSPipe *silence = [NSPipe pipe];
+
+    [otoolTask setLaunchPath: otoolPath];
+    [otoolTask setStandardInput: [NSPipe pipe]];
+    [otoolTask setStandardOutput: silence];
+    [otoolTask setStandardError: silence];
+    [otoolTask launch];
+    [otoolTask waitUntilExit];
+
+    return ([otoolTask terminationStatus] == 1);
 }
 
 @end
